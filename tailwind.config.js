46e7065b6f6b1a842129c284ec8b835c4bcb0ff0
Nina <nina.micleusanu93@gmail.com>
2022-01-26 19:43:30 +0100

    Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,48 @@
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    colors: {
+      "soft-red": "hsl(7, 99%, 70%)",
+      "dark-desaturated-cyan": "hsl(167, 40%, 24%)", //(graphic design text)
+      "dark-blue": "hsl(198, 62%, 26%)", //(photography text)
+      "dark-moderate-cyan": "hsl(168, 34%, 41%)", //(footer)
+      "very-dark-desaturated-blue": "hsl(212, 27%, 19%)",
+      yellow: "hsl(51, 100%, 49%)",
+      "very-dark-grayish-blue": "hsl(213, 9%, 39%)",
+      "grayish-blue": "hsl(210, 4%, 67%)",
+      "dark-grayish-blue": "hsl(232, 10%, 55%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
+    fontFamily: {
+      "barlow-sans": ["Barlow", "sans-serif"],
+      "fraunces-serif": ["Fraunces", "serif"],
+    },
   },
   plugins: [],
-}
+};
